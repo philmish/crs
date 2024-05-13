c34388f95c1e13139a27bda175c267f8dc90cbbb
philmish <thantalosdream@protonmail.com>
2024-05-14 00:38:34 +0200

    feat: pass cli args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::{
-    fs,
+    env, fs,
+    io::ErrorKind,
+    process::{Command, Stdio},
     thread::sleep,
     time::{Duration, SystemTime},
 };
 
 struct WatchedFile {
     path: String,
-    accessed: SystemTime,
+    modified: SystemTime,
 }
 
 impl WatchedFile {
@@ -14,30 +16,57 @@ impl WatchedFile {
         let meta = fs::metadata(path.clone())?;
         Ok(WatchedFile {
             path: path.clone(),
-            accessed: meta.modified()?,
+            modified: meta.modified()?,
         })
     }
 
-    fn was_modified(&mut self) -> std::io::Result<bool> {
-        Ok(std::fs::metadata(self.path.clone())?.modified()? > self.accessed)
+    fn check_modification(&mut self) -> std::io::Result<bool> {
+        let modified = std::fs::metadata(self.path.clone())?.modified()?;
+        if modified > self.modified {
+            self.modified = modified;
+            return Ok(true);
+        }
+        Ok(false)
     }
 }
 
 fn main() -> std::io::Result<()> {
-    let fname = "test.txt";
-    let mut counter = 0;
+    let argv: Vec<String> = env::args().collect();
+    if argv.len() < 4 {
+        println!(
+            "Not enough args provided, expected at least 4 got {}",
+            argv.len()
+        );
+        return Err(std::io::Error::new(
+            ErrorKind::InvalidInput,
+            "Not enough args",
+        ));
+    };
+    let fname = &argv[1];
     let mut f = WatchedFile::new(fname.to_string())?;
-    sleep(Duration::new(2, 0));
+    let cmd_string = &argv[2].clone();
+    let args: Vec<String> = argv
+        .into_iter()
+        .enumerate()
+        .filter(|&(i, _)| i > 2)
+        .map(|(_, a)| a)
+        .collect();
     loop {
-        counter += 1;
-        if f.was_modified()? {
-            println!("Was modified !");
-            f = WatchedFile::new(fname.to_string())?;
+        match f.check_modification() {
+            Ok(modified) => {
+                if modified {
+                    println!("Modified!");
+                    let mut cmd = Command::new(cmd_string)
+                        .args(args.clone())
+                        .stdout(Stdio::inherit())
+                        .stderr(Stdio::inherit())
+                        .spawn()?;
+                    cmd.wait()?;
+                }
+            }
+            Err(_) => break,
         }
         sleep(Duration::new(2, 0));
-        if counter == 100 {
-            break;
-        };
     }
     Ok(())
 }
